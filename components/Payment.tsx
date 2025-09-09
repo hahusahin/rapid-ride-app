bd623eccc809f696d3384e704ce273b5220266c5
H.Huseyin Sahin <huseyin.sahin.3321@gmail.com>
2025-09-09 17:26:30 +0300

    feat: stripe integration

--- components/Payment.tsx
+++ components/Payment.tsx
@@ -1,8 +1,134 @@
-import React from "react";
+import { fetchAPI } from "@/lib/fetch";
+import { useLocationStore } from "@/store";
+import { PaymentProps } from "@/types/type";
+import { useAuth } from "@clerk/clerk-expo";
+import {
+  IntentCreationCallbackParams,
+  PaymentMethod,
+  useStripe,
+} from "@stripe/stripe-react-native";
+import React, { useEffect, useState } from "react";
+import { Alert } from "react-native";
 import CustomButton from "./CustomButton";
 
-const Payment = () => {
-  const openPaymentSheet = () => {};
+const Payment = ({
+  fullName,
+  email,
+  amount,
+  driverId,
+  rideTime,
+}: PaymentProps) => {
+  const { initPaymentSheet, presentPaymentSheet } = useStripe();
+  const {
+    userAddress,
+    userLongitude,
+    userLatitude,
+    destinationLatitude,
+    destinationAddress,
+    destinationLongitude,
+  } = useLocationStore();
+  const { userId } = useAuth();
+  const [success, setSuccess] = useState(false);
+
+  const confirmHandler = async (
+    paymentMethod: PaymentMethod.Result,
+    _: boolean,
+    intentCreationCallback: (params: IntentCreationCallbackParams) => void,
+  ) => {
+    const { paymentIntent, customer } = await fetchAPI(
+      "/(api)/(stripe)/create",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: fullName || email.split("@")[0],
+          email: email,
+          amount: amount,
+          paymentMethodId: paymentMethod.id,
+        }),
+      },
+    );
+    if (paymentIntent.client_secret) {
+      const { result } = await fetchAPI("/(api)/(stripe)/pay", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          payment_method_id: paymentMethod.id,
+          payment_intent_id: paymentIntent.id,
+          customer_id: customer,
+          client_secret: paymentIntent.client_secret,
+        }),
+      });
+
+      if (result.client_secret) {
+        await fetchAPI("/(api)/ride/create", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            origin_address: userAddress,
+            destination_address: destinationAddress,
+            origin_latitude: userLatitude,
+            origin_longitude: userLongitude,
+            destination_latitude: destinationLatitude,
+            destination_longitude: destinationLongitude,
+            ride_time: rideTime.toFixed(0),
+            fare_price: parseInt(amount) * 100,
+            payment_status: "paid",
+            driver_id: driverId,
+            user_id: userId,
+          }),
+        });
+
+        intentCreationCallback({
+          clientSecret: result.client_secret,
+        });
+      }
+    }
+  };
+
+  const initializePaymentSheet = async () => {
+    const { error } = await initPaymentSheet({
+      merchantDisplayName: "Example, Inc.",
+      intentConfiguration: {
+        mode: {
+          amount: 1099,
+          currencyCode: "USD",
+        },
+        confirmHandler: confirmHandler,
+      },
+      returnURL: "rapidrideapp://book-ride",
+    });
+    if (error) {
+      // handle error
+    }
+  };
+
+  useEffect(() => {
+    initializePaymentSheet();
+  }, []);
+
+  const didTapCheckoutButton = async () => {
+    // implement later
+  };
+
+  const openPaymentSheet = async () => {
+    await initializePaymentSheet();
+
+    const { error } = await presentPaymentSheet();
+
+    if (error) {
+      Alert.alert(`Error code: ${error.code}`, error.message);
+    } else {
+      setSuccess(true);
+    }
+  };
+
   return (
     <>
       <CustomButton
